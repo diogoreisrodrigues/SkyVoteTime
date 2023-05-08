c3c07530e0c971074f058d1d3fbaa2914347b50d
artur <artursfreitas@hotmail.com>
2023-05-08 22:19:11 +0100

    build: category checkbox working

--- Server/Migrations/20230508202127_migJSON2.Designer.cs
+++ Server/Migrations/20230508202127_migJSON2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SkyVoteTime.Server.Data;
 
@@ -11,9 +12,11 @@ using SkyVoteTime.Server.Data;
 namespace SkyVoteTime.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230508202127_migJSON2")]
+    partial class migJSON2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shared/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using SkyVoteTime.Shared.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace SkyVoteTime.Shared.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
