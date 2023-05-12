d63b01f070789391efd57d65a926142a4f60a207
artur <artursfreitas@hotmail.com>
2023-05-12 02:08:45 +0100

    build: Delete movies from a competition, not working for now...

---
+++ Server/Controllers/MovieController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkyVoteTime.Server.Service;
+using SkyVoteTime.Server.Models;
+
+
+namespace SkyVoteTime.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController: ControllerBase
+    {
+        private readonly IMovieService _movieService;
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        [HttpGet]
+        public async Task<List<Movie>> GetAll()
+        {
+            return await _movieService.GetAllMovies();
+        }
+        [HttpGet("{id}")]
+        public async Task<Movie> Get(int id)
+        {
+            return await _movieService.GetMovie(id);
+        }
+        [HttpPost]
+        public async Task<Movie> AddMovie([FromBody] Movie movie)
+        {
+            return await _movieService.AddMovie(movie);
+        }
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteMovie(int id)
+        {
+            await _movieService.DeleteMovie(id);
+            return true;
+        }
+        [HttpPut("{id}")]
+        public async Task<bool> UpdateMovie(int id, [FromBody] Movie Object)
+        {
+            await _movieService.UpdateMovie(id, Object);
+            return true;
+        }
+    }
+}
